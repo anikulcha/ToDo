a5a3388edbcf2788433ee6af83a4c6652a9b8646
andrey <nikulcha1998@gmail.com>
2022-12-30 17:35:52 +0200

    finish

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,26 @@
+function createElement (tag, props, ...children){
+    const element = document.createElement(tag);
+    Object.keys(props).forEach(key => element[key] = props[key]);
+    console.log(tag, children[0]);
+    console.log(children);
 
+    // for(child in children){
+    //     if(children[child] == undefined){
+    //         child = 'bum'
+    //     }
+    // }
 
+    if(children.length > 0){
+        children.forEach(child => {
+            if(typeof child === 'string'){
+                child = document.createTextNode(child);
+            }
+            element.appendChild(child);
+        })
+    }
+    return element;
+}
+document.querySelector('h1').textContent = 'хуй.js';
 const todoForm = document.getElementById('todo-form');
 const addInput = document.getElementById('add-input');
 const todoList = document.getElementById('todo-list');
@@ -7,38 +28,14 @@ const todoItems = document.querySelectorAll('.todo-item');
 
 
 function createTodoItem(title){
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'checkbox';
-    
-    const label = document.createElement('label');
-    label.innerText = title;
-    // label.createAttribute('data-');
-    label.className = 'title';
+    const checkbox      = createElement('input', {type: 'checkbox', className: 'checkbox'});   
+    const label         = createElement('label', {className: 'title' }, title);
+    const editInput     = createElement('input', {type: 'text', className: 'textfield'});
+    const editButton    = createElement('button', {className: 'edit'},'Изменить');
+    const deleteButton  = createElement('button', {className: 'delete'},'Удалить');
+    const listItem      = createElement('li',{className:'todo-item'}, checkbox, label, editInput, editButton, deleteButton);
 
-    const editInput = document.createElement('input');
-    editInput.type = 'text';
-    editInput.className = 'textfield';
-
-    const editButton = document.createElement('button');
-    editButton.innerText = 'Изменить';
-    editButton.className = 'edit';
-
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Удалить';
-    deleteButton.className = 'delete';
-
-    const listItem = document.createElement('li');
-    // listItem.innerText = 'Удалить';
-    listItem.className = 'todo-item';
-
-    listItem.appendChild(checkbox);
-    listItem.appendChild(label);
-    listItem.appendChild(editInput);
-    listItem.appendChild(editButton);
-    listItem.appendChild(deleteButton);
-    
-    bindEvents(listItem)
+    bindEvents(listItem);
 
     return listItem;
 
